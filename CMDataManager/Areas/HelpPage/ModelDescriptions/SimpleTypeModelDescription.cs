adaf7b1d9a8de0b2dd2eeaa531645c767ebd79b4
Fernando Cortez <fernandohcortez@gmail.com>
2019-02-28 12:50:17 -0300

    Adicionado WebApi Project

---
+++ CMDataManager/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CMDataManager.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ CMDataManager/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CMDataManager.WebApiApplication" Language="C#" %>
